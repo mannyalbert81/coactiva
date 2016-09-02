8399b30b4f7897f35a3d427b44dc2dfc3c0917b8
Steven <Steven@STEVEN_MASOFT>
2016-09-02 09:49:32 -0500

    fdsfsdfds

--- FrameworkMVC/view/ireports/ContRazonAvocoReport.php
+++ FrameworkMVC/view/ireports/ContRazonAvocoReport.php
@@ -4,12 +4,10 @@
 #Importas la librer�a PhpJasperLibrary
 include_once('PhpJasperLibrary/class/tcpdf/tcpdf.php');
 include_once("PhpJasperLibrary/class/PHPJasperXML.inc.php");
-require_once("library/tcpdf/examples/lang/eng.php");
-require_once("library/tcpdf/tcpdf.php");
-require_once("library/tcpdf/tcpdf_config.php");
-require_once("library/fpdi.php");
 include_once ('conexion.php');
-
+include_once("library/tcpdf/examples/lang/eng.php");
+include_once("library/tcpdf/tcpdf.php");
+include_once("library/fpdi.php");
 #Conectas a la base de datos
 $server  = server;
 $user    = user;
@@ -18,26 +16,6 @@ $db      = db;
 $driver  = driver;
 ini_set('display_errors', 0);
 error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
-
-class Pdf_concat extends FPDI {
-	var $files = array();
-
-	function setFiles($files) {
-		$this->files = $files;
-	}
-
-	function concat() {
-		foreach($this->files AS $file) {
-			$pagecount = $this->setSourceFile($file);
-			for ($i = 1; $i <= $pagecount; $i++) {
-				$tplidx = $this->ImportPage($i);
-				$s = $this->getTemplatesize($tplidx);
-				$this->AddPage('P', array($s['w'], $s['h']));
-				$this->useTemplate($tplidx);
-			}
-		}
-	}
-}
 
 $estado=$_GET['estado'];
 
@@ -94,26 +72,10 @@ $estado=$_GET['estado'];
 				$PHPJasperXML->transferDBtoArray($server,$user,$pass,$db, $driver);
 				
 				$PHPJasperXML->outpage("F",$directorio.$nombre.'.pdf');
-			
-				
-				$directorio1 = $_SERVER ['DOCUMENT_ROOT'] . '/documentos/Avoco/';
-				$directorio3 = $_SERVER ['DOCUMENT_ROOT'] . '/documentos/RazonUnida/';
-				
-				$file2merge=array($directorio.$nombre.'.pdf',$directorio1.'Avoco1062.pdf');
-				$pdf = new Pdf_concat();
-				$pdf->setFiles($file2merge);
-				$pdf->concat();
-				$pdf->Output($directorio3.'newfile.pdf', "F");
-			
-								
 
-				echo "<script type='text/javascript'>";
-				echo "window.close()";
-				echo "</script>";
-				exit();
-				
-				
-           }
+		}
 
+			
 ?>
+
 
