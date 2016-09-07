99380f5ee89b9f10368a94ef91de43bd417227fd
Steven <Steven@STEVEN_MASOFT>
2016-09-07 18:44:03 -0500

    56fghfgj

--- FrameworkMVC/view/ireports/ContRazonAvocoReport.php
+++ FrameworkMVC/view/ireports/ContRazonAvocoReport.php
@@ -16,6 +16,28 @@ $driver  = driver;
 ini_set('display_errors', 0);
 error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
 
+class Pdf_concat extends FPDI {
+	var $files = array();
+
+	function setFiles($files) {
+		$this->files = $files;
+
+			
+		function concat() {
+			foreach($this->files AS $file) {
+				$pagecount = $this->setSourceFile($file);
+				for ($i = 1; $i <= $pagecount; $i++) {
+					$tplidx = $this->ImportPage($i);
+					$s = $this->getTemplatesize($tplidx);
+					$this->AddPage('P', array($s['w'], $s['h']));
+					$this->useTemplate($tplidx);
+				}
+			}
+		}
+	}
+}
+
+
 $estado=$_GET['estado'];
 
 		if ($estado == 'Visualizar') 
@@ -46,7 +68,7 @@ $estado=$_GET['estado'];
 	
 		} else 
 		{
-	
+		
 				$id= $_GET['identificador'];
 				$nombre=$_GET['nombre'];
 				$nombre_avoco_conocimiento=$_GET['nombre_avoco_conocimiento'];
@@ -63,65 +85,28 @@ $estado=$_GET['estado'];
 				
 				
 				$directorio = $_SERVER ['DOCUMENT_ROOT'] . '/documentos/RazonAvoco/';
-	
+				$directorio1 = $_SERVER ['DOCUMENT_ROOT'] . '/documentos/Avoco/';
+				$directorio3 = $_SERVER ['DOCUMENT_ROOT'] . '/documentos/RazonAvocoUnida/';
+				
 				$PHPJasperXML = new PHPJasperXML();
-				
 				$PHPJasperXML->arrayParameter=array("_sql" => $sql);
-				
 				$PHPJasperXML->load_xml_file("RazonAvocoGuardarReport.jrxml");
-				
 				$PHPJasperXML->transferDBtoArray($server,$user,$pass,$db, $driver);
-				
 				$PHPJasperXML->outpage("F",$directorio.$nombre.'.pdf');
-
+		}			
 				
-				
-				
-				
-					
-		}
-		class Pdf_concat extends FPDI {
-			var $files = array();	
-			 
-			function setFiles($files) {
-				$this->files = $files;
-			}
 		
-			function concat() {
-				foreach($this->files AS $file) {
-					$pagecount = $this->setSourceFile($file);
-					for ($i = 1; $i <= $pagecount; $i++) {
-						$tplidx = $this->ImportPage($i);
-						$s = $this->getTemplatesize($tplidx);
-						$this->AddPage('P', array($s['w'], $s['h']));
-						$this->useTemplate($tplidx);
-					}
-				}
-			}
-		}
-		$directorio1 = $_SERVER ['DOCUMENT_ROOT'] . '/documentos/Avoco/';
-		$directorio3 = $_SERVER ['DOCUMENT_ROOT'] . '/documentos/RazonAvocoUnida/';
 		
-		$file2merge=array($directorio1.$nombre_avoco_conocimiento.'.pdf', $directorio.$nombre.'.pdf');
+		$file2merge=array($directorio1.'Avoco1073.pdf', $directorio.'RazonAvoco1090.pdf');
 		$pdf = new Pdf_concat();
 		$pdf->setFiles($file2merge);
 		$pdf->concat();
-		$pdf->Output($directorio3.'RazonAvocoUnido'.$identificador_avoco_unido.'.pdf', "F");
-		
-		$dir=($directorio.$nombre.'.pdf'); 
-		if(file_exists($dir))
-		{
-			if(unlink($dir))
-				print "El archivo fue borrado";
-		}
-		else
-			print "Este archivo no existe";
-		
+		$pdf->Output($directorio3.'RazonAvocoUnida.pdf', "F");
+
 		echo "<script type='text/javascript'>";
 		echo "window.close()";
 		echo "</script>";
 		exit();
-
 			
 ?>
 
