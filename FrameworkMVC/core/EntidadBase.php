ae7df0e6401c25d20e83554a25f217f0f7ddffec
Danny_Masoft <Masoft@DESKTOP-LO1S29T>
2016-09-12 18:11:33 -0500

    comit

--- FrameworkMVC/core/EntidadBase.php
+++ FrameworkMVC/core/EntidadBase.php
@@ -533,6 +533,8 @@ class EntidadBase{
     	return $resultado;
     }
     
+    
+    
     public function FirmarDocumentos($directorio,$nombrePdf,$id_firma)
     {
     	@@session_start();
@@ -642,6 +644,21 @@ class EntidadBase{
     	 
     	return $resultado;
     }
+
+    public function extraerEspacio($filePath,$addpage)
+    {
+    	$ruta_exec_extract_line = $_SERVER['DOCUMENT_ROOT'].'/documentos/extraer/VerEspacioPdf.exe';
+    	
+    	$comando_extraer = 'start "" /b "' . $ruta_exec_extract_line . '" ' . $filePath . ' '. $addpage . ' ';
+    	 
+    	$comando_esc_extraer = escapeshellcmd ( $comando_extraer );
+    	 
+    	exec ( $comando_esc_extraer, $resultadoExtraer, $ejecucion_extraer );
+    	 
+    	$total_espacio=$resultadoExtraer[0];
+    	
+    	return $total_espacio;
+    }
     
     public function FirmarPDFs($destino,$nombrePdf,$id_firma,$id_rol,$id_usuario=null)
     {
@@ -659,8 +676,24 @@ class EntidadBase{
     	
     	$origen=$moveTo;
     	$destino=$moveOf;
+    	$tipo_pdf="";
+    	$add_page="false";
+    	$total_espacio=0;
+    	//error en la conversion
+    	$nombre_pdf_bd = $origen;
     	
-    	$comando = 'start "" /b "' . $ruta_ejecutable . '" ' . $id_firma . ' ' . $origen . ' ' . $destino . ' '.$id_rol.' '.$id_usuario.' ';
+    	$conAddPage=$this->getCondiciones("nombre_pdf,tipo_pdf", "pdf", "nombre_pdf='$nombre_pdf_bd'", "id_pdf");
+    	
+    	if(!empty($conAddPage)){
+    		$add_page="true";
+    		$total_espacio=$this->extraerEspacio($origen, $add_page);
+    	}else {
+    		$total_espacio=$this->extraerEspacio($origen, $add_page);
+    	}
+    	
+    	
+    	$comando = 'start "" /b "' . $ruta_ejecutable . '" ' . $id_firma . ' ' . $origen . ' ' . $destino . ' '.$id_rol.
+    				' '.$id_usuario.' '.$total_espacio.' '.$add_page.' ';
     
     	$comando_esc = escapeshellcmd ( $comando );
     
