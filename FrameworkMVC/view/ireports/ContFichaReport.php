c7bcd825e216cd7d615b2eb329517e65a2914362
Danny_Masoft <Masoft@DESKTOP-LO1S29T>
2016-09-19 12:47:47 -0500

    lunes

--- FrameworkMVC/view/ireports/ContFichaReport.php
+++ FrameworkMVC/view/ireports/ContFichaReport.php
@@ -1,14 +1,13 @@
 
-
 <?php 
 #<?php 
 #Importas la librer�a PhpJasperLibrary
-//add this line here
-
-include_once('PhpJasperLibrary/class/tcpdf/tcpdf.php');
-include_once("PhpJasperLibrary/class/PHPJasperXML.inc.php");
-
-include_once ('conexion.php');
+ob_end_clean(); //add this line here
+			
+	include_once('PhpJasperLibrary/class/tcpdf/tcpdf.php');
+			include_once("PhpJasperLibrary/class/PHPJasperXML.inc.php");
+			
+			include_once ('conexion.php');
 
 
 #Conectas a la base de datos 
@@ -19,6 +18,7 @@ $db      = db;
 $driver  = driver;
 ini_set('display_errors', 0);
 error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
+$PHPJasperXML->debugsql=false;
 
 #aqu� va el reporte
 
@@ -26,27 +26,16 @@ error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
 $id=$_GET['id_juicios'];
 
 
-			$PHPJasperXML = new PHPJasperXML ();
+			$PHPJasperXML = new PHPJasperXML("en","TCPDF");
 			
 			$PHPJasperXML->debugsql = false;
 		
-		    $PHPJasperXML->arrayParameter=array('_id_juicios'=>19);
+		    $PHPJasperXML->arrayParameter=array('id_juicios_report'=>$id);
 		    
 			$PHPJasperXML->load_xml_file( "FichaVisualizarReport.jrxml" );
 			
 			$PHPJasperXML->transferDBtoArray ( $server, $user, $pass, $db, $driver );
 			
 			$PHPJasperXML->outpage ( "I" );
-			
-			/*$xml = simplexml_load_file("FichaVisualizarReport.jrxml"); // Leemos nuestro reporte de ireport
-			
-			
-			$PHPJasperXML = new PHPJasperXML("en", "TCPDF"); // instanciamos el obj
-			$PHPJasperXML->debugsql=false; // Si deseas ver la setencia del sql del reporte lo pones en true
-			$PHPJasperXML->arrayParameter=array("_id_juicios"=>$id); // el paramentro que enviamos a nuestro reporte
-			$PHPJasperXML->xml_dismantle($xml);
-			$PHPJasperXML->transferDBtoArray($server,$user,$pass,$db); // las opciones de conexion de base de datos
-			$PHPJasperXML->outpage("I"); // I: muetsra en el browser D: descarga el pdf
-*/
-
+		
 ?>
