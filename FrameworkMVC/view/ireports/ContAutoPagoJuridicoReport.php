99cb9499ada2c73f305a24205ecafe130e52e5a9
Danny_Masoft <Masoft@DESKTOP-LO1S29T>
2016-09-26 16:51:27 -0500

    comit

--- FrameworkMVC/view/ireports/ContAutoPagoJuridicoReport.php
+++ FrameworkMVC/view/ireports/ContAutoPagoJuridicoReport.php
@@ -1,11 +1,8 @@
 <?php
-
-#<?php
-#Importas la librer�a PhpJasperLibrary
+ob_start();
 include_once('PhpJasperLibrary/class/tcpdf/tcpdf.php');
 include_once("PhpJasperLibrary/class/PHPJasperXML.inc.php");
 include_once ('conexion.php');
-
 #Conectas a la base de datos
 $server  = server;
 $user    = user;
@@ -48,19 +45,44 @@ $estado=$_GET['estado'];
 	
 				$id= $_GET['identificador'];
 				$nombre=$_GET['nombre'];
+				$directorio = $_SERVER ['DOCUMENT_ROOT'] . '/documentos/AutoPagos/';
 				
-				//aqui va la consulta
-				$sql="SELECT * FROM auto_pagos_report WHERE identificador='$id' ";
+				$sql="SELECT juicios.juicio_referido_titulo_credito,
+						juicios.creado,
+						clientes.nombres_clientes,
+						clientes.identificacion_clientes,
+						clientes.nombre_garantes,
+						clientes.identificacion_garantes,
+						asignacion_secretarios_view.liquidador,
+						asignacion_secretarios_view.secretarios,
+						asignacion_secretarios_view.impulsores,
+						titulo_credito.id_titulo_credito,
+						titulo_credito.total_total_titulo_credito
+					FROM public.asignacion_secretarios_view,
+						public.clientes,
+						public.auto_pagos,
+						public.titulo_credito,
+						public.juicios
+					WHERE 
+						 asignacion_secretarios_view.id_abogado = titulo_credito.id_usuarios 
+						 AND clientes.id_clientes = titulo_credito.id_clientes 
+						 AND titulo_credito.id_titulo_credito = auto_pagos.id_titulo_credito 
+						 AND titulo_credito.id_titulo_credito = juicios.id_titulo_credito 
+						 AND auto_pagos.identificador = '$id' ";
 				
 				$PHPJasperXML = new PHPJasperXML();
 				
-				$PHPJasperXML->arrayParameter=array("sql" => $sql);
+				$PHPJasperXML->debugsql = false;
 				
-				$PHPJasperXML->load_xml_file("AutoPagoGuardar.jrxml");
+				$PHPJasperXML->arrayParameter=array('sql' => $sql);
 				
+				$PHPJasperXML->load_xml_file("AutoPagoJURIDICOCREGARANTEReport.jrxml");
+					
 				$PHPJasperXML->transferDBtoArray($server,$user,$pass,$db, $driver);
+					
+				//$PHPJasperXML->outpage("F",$directorio.$nombre.'.pdf');
+				$PHPJasperXML->outpage("I");
 				
-				$PHPJasperXML->outpage("F",$directorio.$nombre.'.pdf');
 				
 				echo "<script type='text/javascript'>";
 				echo "window.close()";
