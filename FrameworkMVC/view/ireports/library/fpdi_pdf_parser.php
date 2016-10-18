07a51428a6fe9286d101b0755537ad6c6fd2adc4
ManuelAlberto <manuel@masoft.net>
2016-10-17 19:20:03 -0500

    fvf

--- FrameworkMVC/view/ireports/library/fpdi_pdf_parser.php
+++ FrameworkMVC/view/ireports/library/fpdi_pdf_parser.php
@@ -1,12 +1,12 @@
 <?php
-/**
+/*****
  * This file is part of FPDI
  *
  * @package   FPDI
  * @copyright Copyright (c) 2015 Setasign - Jan Slabon (http://www.setasign.com)
  * @license   http://opensource.org/licenses/mit-license The MIT License
  * @version   1.6.1
- */
+ ******/
 
 if (!class_exists('pdf_parser')) {
     require_once('pdf_parser.php');
